1b2ddf665d6abeab08e94200616b307b0471f275
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-10-17 13:49:06 +0200

    finished day 34 challenge

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -25,6 +25,8 @@ struct ContentView: View {
     @State private var currentQuestion: Int = 0
     @State private var gameOver: Bool = false
     
+    @State private var selectedFlag = -1
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -57,6 +59,13 @@ struct ContentView: View {
                         } label: {
                             FlagImage(country: countries[number])
                         }
+                        .rotation3DEffect(
+                            .degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0)
+                        )
+                        .opacity(selectedFlag == number || selectedFlag == -1 ? 1 : 0.25)
+                        .scaleEffect(selectedFlag == -1 ? 1 : selectedFlag == number ? 1.25 : 0.75)
+                        .animation(.default, value: selectedFlag)
+
                     }
                     
                 }
@@ -68,7 +77,7 @@ struct ContentView: View {
                 
                 Spacer()
                 Spacer()
-                Text("Score: \(userScore)")
+                Text("Score: \(userScore), \(selectedFlag)")
                     .foregroundStyle(Color.white)
                     .font(.title.bold())
                 Spacer()
@@ -77,7 +86,7 @@ struct ContentView: View {
             .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue") { }
+            Button("Continue") { askQuestion() }
         } message: {
             Text(scoreTitle == "Correct!" ? "Your score is: \(userScore)" : "No points this time.")
         }
@@ -90,18 +99,17 @@ struct ContentView: View {
     
     func flagTapped(_ number: Int) {
         currentQuestion += 1
+        selectedFlag = number
         if number == correctAnswer {
             scoreTitle = "Correct!"
             userScore += 1
         } else {
             scoreTitle = "Wrong! That's the flag of \(countries[number])."
         }
-        showingScore = true
         if currentQuestion >= 8 {
             gameOver = true
-        } else {
-            askQuestion()
         }
+        showingScore = true
     }
     
     func restartGame() {
@@ -111,6 +119,7 @@ struct ContentView: View {
     }
     
     func askQuestion() {
+        selectedFlag = -1
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
