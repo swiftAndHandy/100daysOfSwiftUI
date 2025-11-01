4c168aa0e09beb2f9864e6ae87ac44078b13a0a0
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-01 15:27:53 +0100

    finished day 44

---
+++ 09_Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Andre Veltens on 31.10.25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
