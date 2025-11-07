6c38724f5fef1c27edf4cd3fa5f38a940b4300f0
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-07 07:38:00 +0100

    feat: finished day 51, reqres is replaced by httpbin

--- 10_CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ 10_CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation: Bool = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -29,13 +32,55 @@ struct CheckoutView: View {
                 Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place order", action: {})
+                Button("Place order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
                     .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {} message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://httpbin.org/post")!
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("reqres-free-v1", forHTTPHeaderField: "api-key")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("Server response: \(responseString)")
+            }
+
+            struct HTTPBinResponse: Codable {
+                    let json: Order
+            }
+
+            let decoded = try JSONDecoder().decode(HTTPBinResponse.self, from: data)
+            let decodedOrder = decoded.json
+
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+            
+        } catch {
+            print("Checkout failed: \(error.localizedDescription)")
+        }
     }
 }
 
--- 10_CupcakeCorner/CupcakeCorner/Order.swift
+++ 10_CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestsEnabled = "specialRequestsEnabled"
+        case _addFrosting = "addFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Chocolate", "Strawberry", "Vanilla", "Pistachio"]
     
     var type: Int = 0
