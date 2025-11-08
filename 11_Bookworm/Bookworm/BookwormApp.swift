74b29f71f2cb912ab6a9cfa7df2d14e4e215778e
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-08 11:19:25 +0100

    prepared for day 54

--- 11_Bookworm/Bookworm/BookwormApp.swift
+++ 11_Bookworm/Bookworm/BookwormApp.swift
@@ -14,6 +14,5 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Student.self)
     }
 }
--- 11_Bookworm/Bookworm/ContentView.swift
+++ 11_Bookworm/Bookworm/ContentView.swift
@@ -9,28 +9,14 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.modelContext) var modelContext
-    @Query var students: [Student]
-    
     var body: some View {
-        NavigationStack {
-            List(students) { student in
-                Text(student.name)
-            }
-            .navigationTitle("Classroom")
-            .toolbar {
-                Button("Add") {
-                    let firstNames = ["Detlef", "Jürgen", "Olaf", "Gesine", "Petra"]
-                    let lastNames = ["Kroes", "Veltens", "Böhnke", "Schulz", "Kaiser"]
-                    
-                    let firstName = firstNames.randomElement()!
-                    let lastName = lastNames.randomElement()!
-                    
-                    let student = Student(id: UUID(), name: "\(firstName) \(lastName)")
-                    modelContext.insert(student)
-                }
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
