02f7af346dde97165bce2c8855fd2d5cd4923621
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-11 14:55:44 +0100

    feat: prepared swiftDataProject (day 57)

---
+++ 12_SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Andre Veltens on 11.11.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
