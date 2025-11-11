0086f4b3e25fda38b643c413ca64948722fd4680
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2025-11-11 15:41:34 +0100

    feat: SwiftDataProject: Edit User added

--- 12_SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ 12_SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Andre Veltens on 11.11.25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
