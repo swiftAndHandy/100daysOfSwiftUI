eacd7bdeae0102f032b8d92c4ccc0c3f8fa0f79b
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2026-02-10 17:32:38 +0100

    init projekt 15: AccessibilitySandbox

---
+++ 15_AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Andre Veltens on 10.02.26.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 15_AccessibilitySandbox/AccessibilitySandboxTests/AccessibilitySandboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxTests.swift
+//  AccessibilitySandboxTests
+//
+//  Created by Andre Veltens on 10.02.26.
+//
+
+import Testing
+@testable import AccessibilitySandbox
+
+struct AccessibilitySandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
