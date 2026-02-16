f5901854d8198c72c792facd3a9c3076a9ddeb95
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-02-16 15:05:15 +0100

    testing tabItems/TabView

---
+++ 16_HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Andre Veltens on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
