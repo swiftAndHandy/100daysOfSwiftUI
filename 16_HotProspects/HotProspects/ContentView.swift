03f36c2419e97a5af98c086ff3aaefb8c126a441
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-02-16 15:55:07 +0100

    end day 80 with contextMenu testing

--- 16_HotProspects/HotProspects/ContentView.swift
+++ 16_HotProspects/HotProspects/ContentView.swift
@@ -8,33 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
+    @State private var backgroundColor = Color.red
     
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
+        VStack {
+            Text("Hello world!")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button("Red", systemImage: isSelectedColor(backgroundColor, newColor: .red)) {
+                        backgroundColor = .red
+                    }
+                    Button("Green", systemImage: isSelectedColor(backgroundColor, newColor: .green)) {
+                        backgroundColor = .green
+                    }
+                    Button("Blue", systemImage: isSelectedColor(backgroundColor, newColor: .blue)) {
+                        backgroundColor = .blue
+                    }
+                }
+        }
         
     }
     
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")
-            let (data, _) = try await URLSession.shared.data(from: url!)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings."
-        }
+    func isSelectedColor(_ color: Color, newColor: Color) -> String {
+        return color == newColor ? "checkmark.circle.fill" : ""
         
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Failed to fetch readings: \(error.localizedDescription)"
-        }
     }
+    
 }
 
 #Preview {
