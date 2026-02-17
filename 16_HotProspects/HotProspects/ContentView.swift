10b66e75895412f03f59f352e74b2102389ccd93
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-02-17 14:37:42 +0100

    init: cleaning from feature testing; clean project

--- 16_HotProspects/HotProspects/ContentView.swift
+++ 16_HotProspects/HotProspects/ContentView.swift
@@ -5,41 +5,13 @@
 //  Created by Andre Veltens on 15.02.26.
 //
 
+import SamplePackage
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
     
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-                    if granted {
-                        print("All set!")
-                    } else if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            .padding(.vertical)
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Katzenklo reinigen."
-                content.subtitle = "Das ist wichtig."
-                content.sound = .default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request) { error in
-                    if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-        }
+        
     }
     
 }
