78367952afb49d9c89f7e4ce1d327698ad59e2f6
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-02-23 14:18:00 +0100

    feature: added FlashZilla

---
+++ 17_Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Andre Veltens on 23.02.26.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
