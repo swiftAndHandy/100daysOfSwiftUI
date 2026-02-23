cfda8827fd05506daafed1ca26d0b4fcccb0af65
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-02-23 14:24:07 +0100

    finished day 86

--- 17_Flashzilla/Flashzilla/ContentView.swift
+++ 17_Flashzilla/Flashzilla/ContentView.swift
@@ -12,30 +12,16 @@ struct ContentView: View {
     @State private var isDragging = false
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                offset = .zero
-                isDragging = false
-            }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        VStack {
+            Text("Hello")
+            Spacer()
+                .frame(height: 100)
+            Text("World")
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            print("VStack tapped!")
+        }
     }
 }
 
