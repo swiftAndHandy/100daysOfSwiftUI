5f77e9a3d3df12d28046e64c220e2f9721722315
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-03-09 15:59:55 +0100

    finishing day 93

--- 18_LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 18_LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -12,22 +12,23 @@ struct ContentView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
                 ForEach(1..<20) { number in
-                    GeometryReader { proxy in
-                        Text("Number \(number)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(
-                                .degrees(-proxy.frame(in: .global).minX / 8),
-                                axis: (x: 0, y: 1, z: 0)
-                            )
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
+                    Text("Number \(number)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                                content
+                                .rotation3DEffect(
+                                    .degrees(-proxy.frame(in: .global).minX / 8),
+                                    axis: (x: 0, y: 1, z: 0)
+                                )
+                        }
                 }
             }
+            .scrollTargetLayout()
         }
-        .padding()
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 
