9cd8ca78780a45c727902c6cb63c28250d07959b
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-03-10 15:38:04 +0100

    init SnowSeeker

---
+++ 19_SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Andre Veltens on 10.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
