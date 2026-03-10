1176d907705aa0d88733d43b584eade4878313ec
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-03-10 14:09:25 +0100

    challenge day 95

---
+++ HighRollers/HighRollers/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  HighRollers
+//
+//  Created by Andre Veltens on 10.03.26.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var value: Int
+    var body: some View {
+        Text("\(value)")
+    }
+}
+
+struct DiceList: View {
+    var results: [Int]
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 50))
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 10) {
+            ForEach(Array(results.enumerated()), id: \.offset) { index, result in
+                DiceView(value: result)
+                    .frame(width: 50, height: 50)
+                    .background(.blue)
+                    .border(.black)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    let diceSides = [4, 6, 8, 10, 12, 20, 100]
+    
+    @State private var amountOfDices: Int = 1
+    @State private var amountOfSides: Int = 6
+    
+    @State private var results = [Int]()
+    
+    var body: some View {
+        Spacer()
+        Text("Result: \(results.reduce(0, +))")
+        DiceList(results: results)
+            .padding()
+            .frame(maxWidth: .infinity)
+        Spacer()
+        HStack {
+            VStack(spacing: 0) {
+                Text("Number of dices:")
+                Picker("Number of dices", selection: $amountOfDices) {
+                    ForEach(1..<10) {
+                        Text("\($0)").tag($0)
+                    }
+                }
+                .pickerStyle(.wheel)
+            }
+            VStack(spacing: 0) {
+                Text("Sides:")
+                Picker("Number of dices", selection: $amountOfSides) {
+                    ForEach(diceSides, id: \.self) {
+                        Text("\($0)").tag(Int($0))
+                    }
+                }
+                .pickerStyle(.wheel)
+            }
+        }
+        Button("Roll \(amountOfDices)w\(amountOfSides)") {
+            rollDice()
+        }
+        .buttonStyle(.glass)
+        Spacer()
+    }
+    
+    func rollDice() {
+        results.removeAll()
+        for _ in 0..<amountOfDices {
+            results.append(Int.random(in: 1...amountOfSides))
+        }
+        print(results)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HighRollers/HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Andre Veltens on 10.03.26.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
